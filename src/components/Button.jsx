63e429350293eacfbd0b028be5538b6596f74202
sangboking <cowg001@naver.com>
2022-10-17 13:33:15 +0900

    fix : 경로명 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
-import { addNumberSelector, numberAtom } from "atom/buttonAtom";
+import { addNumberSelector } from "store/buttonAtom";
 import React from "react";
 import { useRecoilValue } from "recoil";
 
 export default function Button() {
   const number = useRecoilValue(addNumberSelector);
-  return <div>{number}</div>;
+
+  return (
+    <div>
+      {import.meta.env.VITE_TEST}
+      {number}
+    </div>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path, { resolve } from "path";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      port: 3000,
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
