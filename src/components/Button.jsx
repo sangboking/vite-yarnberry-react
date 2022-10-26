f49818f40c0b309651f7c89ba5c08b382c118c96
sangboking <cowg001@naver.com>
2022-10-26 16:05:13 +0900

    test githubaction aws ec2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,14 +6,18 @@ export default function Button() {
   const number = useRecoilValue(addNumberSelector);
 
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      {import.meta.env.VITE_TEST}
-      {number}
-      <Icon name="facebook" width="1.5rem" height="1.5rem" />
-      <Icon name="vite" width="1.5rem" height="1.5rem" />
-      <div>안녕하세요</div>
-      <div>깃헙 액션 테스트</div>
-      <div>s3 - ec2 배포성공</div>
+    <div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        {import.meta.env.VITE_TEST}
+        {number}
+        <Icon name="facebook" width="1.5rem" height="1.5rem" />
+        <Icon name="vite" width="1.5rem" height="1.5rem" />
+        <div>안녕하세요</div>
+        <div>깃헙 액션 테스트</div>
+        <div>s3 - ec2 배포성공</div>
+      </div>
+
+      <div>ec2 자동배포 마지막 테스트</div>
     </div>
   );
 }
